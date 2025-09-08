54afca848aed82c5ba5cf3bbfcd0ca5690399bdd
sameer khan <sameer.krecruitment@gmail.com>
2025-09-08 15:50:47 -0400

    Create init.py

---
+++ advanced-object-detection/tests/init.py
@@ -0,0 +1,9 @@
+"""
+Test module for object detection system
+"""
+
+import sys
+import os
+
+# Add parent directory to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
