a739d3a1863e86f5e029ed3457a337f512d49677
sameer khan <sameer.krecruitment@gmail.com>
2025-09-08 10:40:16 -0400

    Create init.py

---
+++ etl-pipeline-enhanced/tests/init.py
@@ -0,0 +1,9 @@
+"""
+Test module for ETL Pipeline
+"""
+
+import os
+import sys
+
+# Add parent directory to path for imports
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
