33d4593207cbfd43893b940958aded5deba86088
sameer khan <sameer.krecruitment@gmail.com>
2025-09-08 19:21:03 -0400

    Create init.py

---
+++ Cardiovascular Risk Prediction project/src/init.py
@@ -0,0 +1,18 @@
+"""
+Cardiovascular Risk Prediction Source Package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .data_preprocessing import DataPreprocessor
+from .feature_engineering import FeatureEngineer
+from .model_training import ModelTrainer
+from .model_evaluation import ModelEvaluator
+
+__all__ = [
+    'DataPreprocessor',
+    'FeatureEngineer',
+    'ModelTrainer',
+    'ModelEvaluator'
+]
